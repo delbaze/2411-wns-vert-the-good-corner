e24c051a498d9d28efd3309a189d1d4763191776
delbaze <56109313+delbaze@users.noreply.github.com>
2024-11-12 17:35:38 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import adsRouter from "./routes/ads.routes";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/ads", adsRouter);
+
+app.listen(4000, () => {
+  console.log("Le serveur est lancé sur le port 4000");
+});
---
+++ src/types/ads.d.ts
@@ -0,0 +1,9 @@
+export type Ad = {
+    id: string;
+    title: string;
+    description: string;
+    price: number;
+    picture: string;
+    location: string;
+  };
+  
